1163db762b588bef375c83a7519f81e540f59dd4
Tekolo Theka <tekolotheka@gmail.com>
2026-06-28 23:39:59 +0200

    Remove generated app branding badge

--- scaffold/src/pages/Index.tsx
+++ scaffold/src/pages/Index.tsx
@@ -1,6 +1,4 @@
-// Update this page (the content is just a fallback if you fail to update the page)
-
-import { MadeWithDyad } from "@/components/made-with-dyad";
+﻿// Update this page (the content is just a fallback if you fail to update the page)
 
 const Index = () => {
   return (
@@ -11,9 +9,9 @@ const Index = () => {
           Start building your amazing project here!
         </p>
       </div>
-      <MadeWithDyad />
     </div>
   );
 };
 
 export default Index;
+
